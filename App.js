4b5f9152de04b21478520c188f5e8eb7fdfbf259
hase-yi <83903443+hase-yi@users.noreply.github.com>
2023-11-10 11:08:08 +0100

    implemented AC

--- App.js
+++ App.js
@@ -7,221 +7,243 @@ const buttonWidth = (screenWidth - 50) / 4;
 
 
 export default function App() {
-  const [answerValue, setAnswerValue] = useState(0);
-  const [readyToReplace, setReadyToReplace] = useState(true);
-  const [memoryValue, setMemoryValue] = useState(0);
-  const [operatorValue, setOperatorValue] = useState(0);
+	const [answerValue, setAnswerValue] = useState(0);
+	const [readyToReplace, setReadyToReplace] = useState(true);
+	const [memoryValue, setMemoryValue] = useState(0);
+	const [operatorValue, setOperatorValue] = useState(0);
+	const [isAc, setIsAc] = useState(true);
 
-  function handleNumber(value) {
-    if (readyToReplace) {
-      setAnswerValue(value);
-      setReadyToReplace(false);
-    } else {
-      setAnswerValue(answerValue + value);
-    }
-  }
+	const operators = ['+', '-', 'x', '/']
 
-  function calculateEquals() {
-    let previous = parseFloat(memoryValue);
-    let current = parseFloat(answerValue);
-    let result = 0;
-  
-    switch (operatorValue) {
-      case '+':
-        result = previous + current;
-        break;
-      case '-':
-        result = previous + current;
-        break;
-      case 'X':
-        result = previous + current;
-        break;
-      case '/':
-        result = previous + current;
-        break;
-      case '%':
-        result = previous + current;
-      default:
-        result = current;
-    }
-    return result;
-  }
+	function handleNumber(value) {
+		if (readyToReplace) {
+			return value;
+		} else {
+			setAnswerValue(answerValue + value);
+		}
+	}
 
-  function buttonPressed(value) {
-    // alert("button pressed: "+value);
-    if (value === 'C') {
-      setAnswerValue(0);
-      setMemoryValue(0);
-      setOperatorValue(0);
-      setReadyToReplace(true);
-    } else if (!isNaN(value)) {
-      handleNumber(value);
-    } else if ( value === '%' || value === '/' || value === 'X' || value === '-' || value === '+') {
-      
-      if (operatorValue !== 0) {
-        const result = calculateEquals();
-        setMemoryValue(result);
-      }
-      setMemoryValue(answerValue);
-      setOperatorValue(value);
-      setReadyToReplace(true);
-    } else if (value === '=') {
-      setAnswerValue(calculateEquals());
-      setMemoryValue(0);
-      setReadyToReplace(true);
-    }
-  }
+	function calculateEquals() {
+		let previous = parseFloat(memoryValue);
+		let current = parseFloat(answerValue);
+		let result = 0;
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <Text style={styles.resultFieldText}>{answerValue}</Text>
-        <View style={styles.row}>
+		switch (operatorValue) {
+			case '+':
+				result = previous + current;
+				break;
+			case '-':
+				result = previous - current;
+				break;
+			case 'x':
+				result = previous * current;
+				break;
+			case '/':
+				result = previous / current;
+				break;
+			default:
+				result = current;
+		}
+		return result;
+	}
 
-          <TouchableOpacity style={styles.button} onPress={() => buttonPressed('C')}>
-            <Text style={styles.buttonText}>C</Text>
-          </TouchableOpacity>
+	function buttonPressed(value) {
+		// alert("button pressed: " + value);
 
-          <TouchableOpacity style={styles.button} onPress={() => buttonPressed()}>
-            <Text style={styles.buttonText}>+/-</Text>
-          </TouchableOpacity>
+		// check is the user is pressing a number
+		if (!isNaN(value)) {
+		
+			setAnswerValue(handleNumber(value));
+			setIsAc(false);
+		} else if (value === 'C') { //check if the user is pressing C
+			setAnswerValue(0);
+			setReadyToReplace(true);
+			setIsAc(true);
+		} else if (value === 'AC')//check if the user is pressing AC{
+		{
+			setAnswerValue(0);
+			setMemoryValue(0);
+			setOperatorValue(0);
+			setReadyToReplace(true);
 
-          <TouchableOpacity style={styles.button} onPress={() => buttonPressed()}>
-            <Text style={styles.buttonText}>%</Text>
-          </TouchableOpacity>
+		} else if (operators.includes(value)) {
 
-          <TouchableOpacity style={styles.topRowButton} onPress={() => buttonPressed()}>
-            <Text style={styles.buttonText}>/</Text>
-          </TouchableOpacity>
+			// The user continus a calucaltion but does not press = inbetween. e.g 1 + 3 / 6 + 2
+			if (operatorValue !== 0) {
+				// Finish the current calculation
+				let result = calculateEquals();
+				setMemoryValue(result)
+			} else {
+				// moving the previous answer to memory to be used in the next calculation
+				setMemoryValue(answerValue);
+			}
 
-        </View>
+			setReadyToReplace(true);
+			setOperatorValue(value);
+		}
+		else if (value === '=') {
+			setAnswerValue(calculateEquals());
+			setMemoryValue(0);
+			setOperatorValue(0);
+			setReadyToReplace(true);
+		} else if (value === '+/-') {
+			setAnswerValue(answerValue * -1);
+		} else if (value === '%') {
+			setAnswerValue(answerValue * 0.01);
+		} 
+	}
 
-        <View style={styles.row}>
+	return (
+		<SafeAreaView style={styles.container}>
+			<View style={styles.container}>
+				<Text style={styles.resultFieldText}>{answerValue}</Text>
+				<View style={styles.row}>
 
-          <TouchableOpacity style={styles.button} onPress={() => buttonPressed(7)}>
-            <Text style={styles.buttonText}>7</Text>
-          </TouchableOpacity>
+					<TouchableOpacity style={styles.button}
+						onPress={() => buttonPressed('C')}>
+						<Text style={styles.buttonText}>{isAc ? 'AC' : 'C'}</Text>
+					</TouchableOpacity>
 
-          <TouchableOpacity style={styles.button} onPress={() => buttonPressed(8)}>
-            <Text style={styles.buttonText}>8</Text>
-          </TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={() => buttonPressed('+/-')}>
+						<Text style={styles.buttonText}>+/-</Text>
+					</TouchableOpacity>
 
-          <TouchableOpacity style={styles.button} onPress={() => buttonPressed(9)}>
-            <Text style={styles.buttonText}>9</Text>
-          </TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={() => buttonPressed('%')}>
+						<Text style={styles.buttonText}>%</Text>
+					</TouchableOpacity>
 
-          <TouchableOpacity style={styles.topRowButton} onPress={() => buttonPressed("*")}>
-            <Text style={styles.buttonText}>x</Text>
-          </TouchableOpacity>
+					<TouchableOpacity style={styles.topRowButton} onPress={() => buttonPressed('/')}>
+						<Text style={styles.buttonText}>/</Text>
+					</TouchableOpacity>
 
-        </View>
-        <View style={styles.row}>
+				</View>
 
-          <TouchableOpacity style={styles.button} onPress={() => buttonPressed(4)}>
-            <Text style={styles.buttonText}>4</Text>
-          </TouchableOpacity>
+				<View style={styles.row}>
 
-          <TouchableOpacity style={styles.button} onPress={() => buttonPressed(5)}>
-            <Text style={styles.buttonText}>5</Text>
-          </TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={() => buttonPressed(7)}>
+						<Text style={styles.buttonText}>7</Text>
+					</TouchableOpacity>
 
-          <TouchableOpacity style={styles.button} onPress={() => buttonPressed(6)}>
-            <Text style={styles.buttonText}>6</Text>
-          </TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={() => buttonPressed(8)}>
+						<Text style={styles.buttonText}>8</Text>
+					</TouchableOpacity>
 
-          <TouchableOpacity style={styles.topRowButton} onPress={() => buttonPressed("-")}>
-            <Text style={styles.buttonText}>-</Text>
-          </TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={() => buttonPressed(9)}>
+						<Text style={styles.buttonText}>9</Text>
+					</TouchableOpacity>
 
-        </View>
-        <View style={styles.row}>
+					<TouchableOpacity style={styles.topRowButton} onPress={() => buttonPressed('x')}>
+						<Text style={styles.buttonText}>x</Text>
+					</TouchableOpacity>
 
-          <TouchableOpacity style={styles.button} onPress={() => buttonPressed(1)}>
-            <Text style={styles.buttonText}>1</Text>
-          </TouchableOpacity>
+				</View>
+				<View style={styles.row}>
 
-          <TouchableOpacity style={styles.button} onPress={() => buttonPressed(2)}>
-            <Text style={styles.buttonText}>2</Text>
-          </TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={() => buttonPressed(4)}>
+						<Text style={styles.buttonText}>4</Text>
+					</TouchableOpacity>
 
-          <TouchableOpacity style={styles.button} onPress={() => buttonPressed(3)}>
-            <Text style={styles.buttonText}>3</Text>
-          </TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={() => buttonPressed(5)}>
+						<Text style={styles.buttonText}>5</Text>
+					</TouchableOpacity>
 
-          <TouchableOpacity style={styles.topRowButton} onPress={() => buttonPressed("+")}>
-            <Text style={styles.buttonText}>+</Text>
-          </TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={() => buttonPressed(6)}>
+						<Text style={styles.buttonText}>6</Text>
+					</TouchableOpacity>
 
-        </View>
-        <View style={styles.row}>
+					<TouchableOpacity style={styles.topRowButton} onPress={() => buttonPressed('-')}>
+						<Text style={styles.buttonText}>-</Text>
+					</TouchableOpacity>
 
-          <TouchableOpacity style={styles.longButton} onPress={() => buttonPressed(0)}>
-            <Text style={styles.buttonText}>0</Text>
-          </TouchableOpacity>
+				</View>
+				<View style={styles.row}>
 
-          <TouchableOpacity style={styles.button} onPress={() => buttonPressed(".")}>
-            <Text style={styles.buttonText}>.</Text>
-          </TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={() => buttonPressed(1)}>
+						<Text style={styles.buttonText}>1</Text>
+					</TouchableOpacity>
 
-          <TouchableOpacity style={styles.topRowButton} onPress={() => buttonPressed("=")}>
-            <Text style={styles.buttonText}>=</Text>
-          </TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={() => buttonPressed(2)}>
+						<Text style={styles.buttonText}>2</Text>
+					</TouchableOpacity>
 
-        </View>
-        <StatusBar style="light content" />
-      </View>
-    </SafeAreaView>
-  );
+					<TouchableOpacity style={styles.button} onPress={() => buttonPressed(3)}>
+						<Text style={styles.buttonText}>3</Text>
+					</TouchableOpacity>
+
+					<TouchableOpacity style={styles.topRowButton} onPress={() => buttonPressed('+')}>
+						<Text style={styles.buttonText}>+</Text>
+					</TouchableOpacity>
+
+				</View>
+				<View style={styles.row}>
+
+					<TouchableOpacity style={styles.longButton} onPress={() => buttonPressed(0)}>
+						<Text style={styles.buttonText}>0</Text>
+					</TouchableOpacity>
+
+					<TouchableOpacity style={styles.button} onPress={() => buttonPressed('.')}>
+						<Text style={styles.buttonText}>.</Text>
+					</TouchableOpacity>
+
+					<TouchableOpacity style={styles.topRowButton} onPress={() => buttonPressed('=')}>
+						<Text style={styles.buttonText}>=</Text>
+					</TouchableOpacity>
+
+				</View>
+				<StatusBar style="light content" />
+			</View>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-  },
-  resultFieldText: {
-    color: "white",
-    fontSize: 60,
-    margin: 20,
-    textAlign: 'right',
-  },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  button: {
-    backgroundColor: 'lightgray',
-    width: buttonWidth,
-    height: buttonWidth,
-    margin: 5,
-    borderRadius: buttonWidth / 2,
-    padding: 20
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 30,
-    textAlign: 'center',
-  },
-  topRowButton: {
-    backgroundColor: 'skyblue',
-    width: buttonWidth,
-    height: buttonWidth,
-    margin: 5,
-    borderRadius: buttonWidth / 2,
-    padding: 20
+	container: {
+		flex: 1,
+		backgroundColor: 'black',
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+	},
+	resultFieldText: {
+		color: "white",
+		fontSize: 60,
+		margin: 20,
+		textAlign: 'right',
+	},
+	row: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	button: {
+		backgroundColor: 'lightgray',
+		width: buttonWidth,
+		height: buttonWidth,
+		margin: 5,
+		borderRadius: buttonWidth / 2,
+		padding: 20
+	},
+	buttonText: {
+		color: 'white',
+		fontSize: 30,
+		textAlign: 'center',
+	},
+	topRowButton: {
+		backgroundColor: 'skyblue',
+		width: buttonWidth,
+		height: buttonWidth,
+		margin: 5,
+		borderRadius: buttonWidth / 2,
+		padding: 20
 
-  },
-  rightRowButton: {
+	},
+	rightRowButton: {
 
-  },
-  longButton: {
-    backgroundColor: 'lightgray',
-    width: buttonWidth * 2 + 5,
-    height: buttonWidth,
-    margin: 5,
-    borderRadius: buttonWidth / 2,
-    padding: 20
-  }
-});
+	},
+	longButton: {
+		backgroundColor: 'lightgray',
+		width: buttonWidth * 2 + 5,
+		height: buttonWidth,
+		margin: 5,
+		borderRadius: buttonWidth / 2,
+		padding: 20
+	}
+})
